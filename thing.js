d650b9e4f00b448a110b7fc3822577541b8fff22
astra-the-boop <astra.the.boop@icloud.com>
2025-10-02 17:52:42 +0700

    LET'S FUCKING GOOOO

--- thing.js
+++ thing.js
@@ -24,17 +24,21 @@ app.get("/", (req, res) => {
 
     const body = document.querySelector("body");
     const table = document.querySelector("#table");
-    const tr = document.createElement("tr");
-    const td = document.createElement("td");
-    td.textContent = "test";
-    tr.appendChild(td);
-    table.appendChild(tr);
 
-    const h1 = document.createElement("h1");
-    // body.style.backgroundColor = "black";
-    h1.textContent = "DOM Manipulation Works";
-    h1.style.color = "red";
-    document.body.insertBefore(h1, table);
+    for (i = 0; i < parsed.length-1; i++) {
+        const tr = document.createElement("tr");
+        const td = document.createElement("td");
+        td.textContent = parsed[i]["ITEMNAME"];
+        tr.appendChild(td);
+        table.appendChild(tr);
+    }
+
+
+
+    // const h1 = document.createElement("h1");
+    // h1.textContent = "DOM Manipulation Works";
+    // h1.style.color = "red";
+    // document.body.insertBefore(h1, table);
 
     res.send(dom.serialize());
 })
