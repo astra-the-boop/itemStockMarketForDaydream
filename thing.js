eba8cadec8b2a8ded50e2630bb8685bb5702e994
astra-the-boop <astra.the.boop@icloud.com>
2025-10-03 10:31:53 +0700

    fuck school wifi

--- thing.js
+++ thing.js
@@ -6,20 +6,40 @@ const app = express();
 
 const port = 3069
 
-
+let init = [];
+let price = [];
 let parsed = [];
+
 app.set("view engine", "ejs");
+
+function calcPrice(prevPrice, initStock, prevStock, currentStock) {
+    let demand = prevStock - currentStock;
+    let stockRatio = currentStock - initStock;
+
+    let scarcity = 1+ (1-stockRatio)*0.8;
+
+    let demandFactor = 1+ (demand/Math.max(1, initStock)) * 2;
+    let newPrice = prevPrice * scarcity * demandFactor;
+
+    if (demand <= 0){
+        newPrice *= 0.9;
+    }
+    let minPrice = prevPrice * 0.5;
+    let maxPrice = prevPrice *3;
+    return Math.max(minPrice, Math.min(maxPrice, newPrice));
+}
+
 function loadData(){
     parsed = [];
     fs.createReadStream("Untitled Spreadsheet.csv").pipe(csv()).on("data", (row)=>{
         parsed.push(row);
     }).on("end", () => {
-        console.log(parsed)
+        // console.log(parsed)
     });
 }
 
 loadData();
-setInterval(loadData, 10000);
+setInterval(loadData, 5000);
 
 
 app.get("/data", (req, res) => {
@@ -36,6 +56,22 @@ app.get("/", (req, res) => {
 
     const columns = ["ITEMID", "ITEMNAME", "ITEMDESC", "MINPRICE", "MAXPRICE", "STOCK"];
 
+    // console.log(price);
+    if (price.length === 0) {
+        console.log("asdklj")
+        price = [];
+        init = [];
+        for (let i = 0; i < parsed.length - 1; i++) {
+            price.push(0.5);
+            let x = [];
+            for (const col of columns) {
+                x.push(parsed[i][col]);
+            }
+            init.push(x);
+        }
+        // console.log(init);
+        // console.log(price);
+    }
     for (i = 0; i < parsed.length-1; i++) {
         const tr = document.createElement("tr");
 
